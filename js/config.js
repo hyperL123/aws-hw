00466ef0f27bca8b094bfdc062f13b7f4fd24fed
LeonOOO <jiabinhuang077@gmail.com>
2023-02-03 22:01:13 -0700

    ..

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_HWXB19CvM", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "58u83bokcuduejvmksumd1rm6g", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "us-east-1_rz3Y4swX2", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "8nf0h53k7pk3cb4bufdfter8c", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
