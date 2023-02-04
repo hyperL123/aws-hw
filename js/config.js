4e1d661daf3e158b5e745ff0877e2484422970f6
LeonOOO <jiabinhuang077@gmail.com>
2023-02-03 23:25:21 -0700

    ...

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "https://master.d1kor8gmi0mc8c.amplifyapp.com/", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl: "https://master.d1kor8gmi0mc8c.amplifyapp.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 };
